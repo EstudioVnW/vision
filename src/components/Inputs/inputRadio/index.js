632b4a7fb15ddd0781c37a4586d00cd8eaaef32a
daniel <danileg09@gmail.com>
2022-04-20 15:11:09 -0300

    component input-radio

---
+++ src/components/Inputs/inputRadio/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css'
+
+
+function InputRadio() {
+    return(
+    
+    
+      <input type="radio" id="radio"></input>
+  
+    )
+    
+  }
+  
+  
+  export default InputRadio;
