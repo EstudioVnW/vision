16d4206f0dee45e3d66dcde6b5e82b417a39d565
Hiago Da Silva <hiagodasilvaconceicao@gmail.com>
2022-04-22 20:13:31 -0300

    only the responsive remains

--- src/components/BoxBalloon/index.js
+++ src/components/BoxBalloon/index.js
@@ -1,45 +1,40 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
 
 //Styles
 import * as S from "./styles";
 
-export default function BemVindo() {
-  const navigate = useNavigate();
-  
-  const handleClick = () => {
-    console.log('handleClick');
-    navigate('/cadastro-da-organizacao');
-  };
+//Button
+import BtnCadastro from "../Buttons/BtnCadastro";
 
+export default function BemVindo() {
   return (
     <S.Container>
-      <S.BoxOne>
-        <S.Paragraph>E aí, tudo tranquilo?</S.Paragraph>
-        <S.Paragraph>
-          <a href="#">Eu sou a Vi,</a> a assistente virtual do Vision.
-        </S.Paragraph>
-      </S.BoxOne>
-      <S.BoxTwo>
+      <S.Box>
+        <S.BoxVi>
+          <S.Paragraph>E aí, tudo tranquilo?</S.Paragraph>
+          <S.Paragraph>
+            <a href="#">Eu sou a Vi,</a> a assistente virtual do Vision.
+          </S.Paragraph>
+        </S.BoxVi>
+      </S.Box>
+      <S.Box>
         <S.Paragraph>
           Todas as informações que você colocar aqui são para o cadastro da sua
           ONG na plataforma e poderão te qualificar para as oportunidades
           oferecidas pelo Vision.
         </S.Paragraph>
-      </S.BoxTwo>
-      <S.BoxThree>
+      </S.Box>
+      <S.Box>
         <S.Paragraph>
           Pode ficar tranquilo, só a gente terá acesso a suas informações.
         </S.Paragraph>
-      </S.BoxThree>
-      <S.BoxFor>
+      </S.Box>
+      <S.Box>
         <S.Paragraph>
           Então preencha com bastante atenção. Vamos lá!
         </S.Paragraph>
-      </S.BoxFor>
-      <S.BoxButton>
-        <S.BtnCadastro onClick={handleClick}>Iniciar cadastro</S.BtnCadastro>
-      </S.BoxButton>
+      </S.Box>
+      <BtnCadastro />
     </S.Container>
   );
 }
