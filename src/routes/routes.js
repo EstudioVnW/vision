5b03b210059b39235b5edfebb6ca37e1c7eddd4b
Evelyn Mendes <evelyn.laurindo@gmail.com>
2022-04-26 10:30:20 -0300

    merge

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,19 +11,19 @@ import OngForm from '../pages/Onboarding/OngFormScreen';
 import CauseAndPeopleProfileForm from '../pages/Onboarding/CauseAndPeopleProfileFormScreen';
 
 const AppRoutes = () => {
-	return (
-		<Router>
-			<Routes>
-			  <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/cadastro-da-organizacao" element={<OngForm/>}/>
-        <Route exact path="/cadastro-das-causas" element={<CauseAndPeopleProfileForm/>}/>
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cadastro-da-organizacao" element={<OngForm />} />
+        <Route exact path="/cadastro-das-causas" element={<CauseAndPeopleProfileForm />} />
 
-				{/* <UnauthenticatedRoute exact path='/' component={Home} />
+        {/* <UnauthenticatedRoute exact path='/' component={Home} />
 				<UnauthenticatedRoute exact path='/cadastro-da-organizacao' component={OngFormScreen} /> */}
-				{/* <PrivateRoute exact path='/config/precisa-de-ajuda' component={NeedHelp} /> */}
-			</Routes>
-		</Router>
-	)
+        {/* <PrivateRoute exact path='/config/precisa-de-ajuda' component={NeedHelp} /> */}
+      </Routes>
+    </Router>
+  )
 };
 
 export default AppRoutes;
